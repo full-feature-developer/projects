cd1e0ace8bb0b468ae989949413bf1e6dd7f0688
Mac Lindsay <mldev91@gmail.com>
2023-07-13 12:26:45 -0400

    changed some references

--- temp_receive.py
+++ temp_receive.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from flask import Flask, request, render_template
 from flask_cors import CORS
 import requests
@@ -64,7 +65,7 @@ def receive_temperature():
 def home():
     location, condition, temperature = get_weather()
     news_title, news_link, news_image = get_news()
-    return render_template('index.html', location=location, condition=condition, temperature=temperature, news_title=news_title, news_link=news_link, news_image=news_image, pool_temperature=p_temp)
+    return render_template('index.html', location=location, condition=condition, temperature=temperature, news_title=news_title, news_link=news_link, news_image=news_image)
 
 
 if __name__ == "__main__":
